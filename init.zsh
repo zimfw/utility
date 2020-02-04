c542837cf440cb67218be9192e9c5366d0a3803b
Eric Nielsen <eric@amalgamar.com.br>
2020-02-04 15:43:12 -0500

    Use short `if` form instead of `&&`
    See
    http://zsh.sourceforge.net/Doc/Release/Shell-Grammar.html#Alternate-Forms-For-Complex-Commands
    
    Also no need to test for PAGER as it's set in the beginning of the script.

--- init.zsh
+++ init.zsh
@@ -31,8 +31,8 @@ if (( terminfo[colors] >= 8 )); then
   else
     # BSD
 
-    (( ! ${+CLICOLOR} )) && export CLICOLOR=1
-    (( ! ${+LSCOLORS} )) && export LSCOLORS='ExfxcxdxbxGxDxabagacad'
+    if (( ! ${+CLICOLOR} )) export CLICOLOR=1
+    if (( ! ${+LSCOLORS} )) export LSCOLORS='ExfxcxdxbxGxDxabagacad'
 
     # stock OpenBSD ls does not support colors at all, but colorls does.
     if [[ ${OSTYPE} == openbsd* && ${+commands[colorls]} -ne 0 ]]; then
@@ -41,23 +41,23 @@ if (( terminfo[colors] >= 8 )); then
   fi
 
   # grep Colours
-  (( ! ${+GREP_COLOR} )) && export GREP_COLOR='37;45'               #BSD
-  (( ! ${+GREP_COLORS} )) && export GREP_COLORS="mt=${GREP_COLOR}"  #GNU
+  if (( ! ${+GREP_COLOR} )) export GREP_COLOR='37;45'               #BSD
+  if (( ! ${+GREP_COLORS} )) export GREP_COLORS="mt=${GREP_COLOR}"  #GNU
   if [[ ${OSTYPE} == openbsd* ]]; then
-    (( ${+commands[ggrep]} )) && alias grep='ggrep --color=auto'
+    if (( ${+commands[ggrep]} )) alias grep='ggrep --color=auto'
   else
    alias grep='grep --color=auto'
   fi
 
   # less Colours
   if [[ ${PAGER} == 'less' ]]; then
-    (( ! ${+LESS_TERMCAP_mb} )) && export LESS_TERMCAP_mb=$'\E[1;31m'   # Begins blinking.
-    (( ! ${+LESS_TERMCAP_md} )) && export LESS_TERMCAP_md=$'\E[1;31m'   # Begins bold.
-    (( ! ${+LESS_TERMCAP_me} )) && export LESS_TERMCAP_me=$'\E[0m'      # Ends mode.
-    (( ! ${+LESS_TERMCAP_se} )) && export LESS_TERMCAP_se=$'\E[27m'     # Ends standout-mode.
-    (( ! ${+LESS_TERMCAP_so} )) && export LESS_TERMCAP_so=$'\E[7m'      # Begins standout-mode.
-    (( ! ${+LESS_TERMCAP_ue} )) && export LESS_TERMCAP_ue=$'\E[0m'      # Ends underline.
-    (( ! ${+LESS_TERMCAP_us} )) && export LESS_TERMCAP_us=$'\E[1;32m'   # Begins underline.
+    if (( ! ${+LESS_TERMCAP_mb} )) export LESS_TERMCAP_mb=$'\E[1;31m'   # Begins blinking.
+    if (( ! ${+LESS_TERMCAP_md} )) export LESS_TERMCAP_md=$'\E[1;31m'   # Begins bold.
+    if (( ! ${+LESS_TERMCAP_me} )) export LESS_TERMCAP_me=$'\E[0m'      # Ends mode.
+    if (( ! ${+LESS_TERMCAP_se} )) export LESS_TERMCAP_se=$'\E[27m'     # Ends standout-mode.
+    if (( ! ${+LESS_TERMCAP_so} )) export LESS_TERMCAP_so=$'\E[7m'      # Begins standout-mode.
+    if (( ! ${+LESS_TERMCAP_ue} )) export LESS_TERMCAP_ue=$'\E[0m'      # Ends underline.
+    if (( ! ${+LESS_TERMCAP_us} )) export LESS_TERMCAP_us=$'\E[1;32m'   # Begins underline.
   fi
 fi
 
@@ -68,7 +68,7 @@ fi
 
 alias ll='ls -lh'         # long format and human-readable sizes
 alias l='ll -A'           # long format, all files
-[[ -n ${PAGER} ]] && alias lm="l | ${PAGER}" # long format, all files, use pager
+alias lm="l | ${PAGER}"   # long format, all files, use pager
 alias lr='ll -R'          # long format, recursive
 alias lk='ll -Sr'         # long format, largest file size last
 alias lt='ll -tr'         # long format, newest modification time last
